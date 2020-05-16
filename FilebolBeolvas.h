250ede9ae130a3ab8d318156f8e71d66659191de
gmarci11 <42243742+gmarci11@users.noreply.github.com>
2020-05-16 14:56:35 +0200

    Egyenes, Kor, Parabol beolvaso

--- FilebolBeolvas.h
+++ FilebolBeolvas.h
@@ -27,9 +27,58 @@ vector<Egyenes*> egyenes_beolvaso (string file_nev) {
         int index = 0;
 
         while (file >> x1 >> y1 >> x2 >> y2) {
-            Pont A(x1, y1);
-            Pont B(x2, y2);
-            ret[index++] = new Egyenes (A,B);
+           // cout << "BEOLVASOK" << endl;
+            ret.push_back(new  Egyenes(x1, y1, x2, y2));
+        }
+    }
+
+    file.close();
+    return ret;
+
+}
+
+vector<Kor*> kor_beolvaso (string file_nev) {
+    
+    vector<Kor*> ret;
+    
+    ifstream file;
+    file.open(file_nev);
+
+    if(file.is_open()) {
+        double x1;
+        double y1;
+        double sugar;
+
+        int index = 0;
+
+        while (file >> x1 >> y1 >> sugar) {
+            cout << "BEOLVASOK" << endl;
+            ret.push_back(new  Kor(x1, y1, sugar));
+        }
+    }
+
+    file.close();
+    return ret;
+
+}
+
+vector<Parabola*> parabola_beolvaso (string file_nev) {
+    
+    vector<Parabola*> ret;
+    
+    ifstream file;
+    file.open(file_nev);
+
+    if(file.is_open()) {
+        double x1;
+        double y1;
+        double sugar;
+
+        int index = 0;
+
+        while (file >> x1 >> y1 >> sugar) {
+            cout << "BEOLVASOK" << endl;
+            ret.push_back(new Parabola(x1, y1, sugar));
         }
     }
 
